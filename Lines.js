9e580dd25242941967bf9cb1e23ebd09128e29ca
omiamador <omipibebb@gmail.com>
2026-03-24 08:25:30 -0600

    Add files via upload

---
+++ Lines.js
@@ -0,0 +1,20 @@
+// Archivo de configuración de líneas y rubros
+const proyects_list = [
+    "L23XX",
+    "A2XX",
+    "TAYRON",
+    "F150",
+     "FORD",
+
+
+];
+
+const rubros_list = [
+    "Mecánidsdsdsco",
+    "Eléctrico",
+    "Software",
+    "Calidad",
+    "Cambio Modelo",
+    "Mantenimiento",
+    "Logística"
+];
